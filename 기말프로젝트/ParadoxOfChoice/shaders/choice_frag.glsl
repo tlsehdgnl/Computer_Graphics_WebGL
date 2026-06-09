e29a1461cb420d81d8bfb6010a742d38cc26f9d7
tlsehdgnl <tlse063@gmail.com>
2026-06-10 04:10:49 +0900

    이미지

--- 기말프로젝트/ParadoxOfChoice/shaders/choice_frag.glsl
+++ 기말프로젝트/ParadoxOfChoice/shaders/choice_frag.glsl
@@ -2,6 +2,7 @@
 precision mediump float;
 
 in vec3 fWorldPos;
+in vec2 fTexCoord;
 
 uniform vec3 eyePos;
 
@@ -9,6 +10,11 @@ uniform vec3 objectColor;
 
 uniform float fogStart;
 uniform float fogEnd;
+
+uniform sampler2D texImage;
+uniform sampler2D aoImage;
+uniform bool useTexture;
+uniform bool useAO;
 
 out vec4 fragColor;
 
@@ -29,14 +35,40 @@ void main()
 
     vec3 fogColor =
         vec3(
-            0.05,
-            0.05,
-            0.05
+            0.02,
+            0.03,
+            0.04
         );
+
+    vec3 ambient = vec3(0.08);
+
+    vec3 baseColor;
+
+    if(useTexture)
+    {
+        vec3 diffuse =
+            texture(texImage, fTexCoord).rgb;
+
+        vec3 ao = useAO ?
+            texture(
+                aoImage,
+                fTexCoord
+            ).rgb :
+            vec3(1.0);
+
+        baseColor =
+            diffuse * ao;
+    }
+    else
+    {
+        baseColor = objectColor;
+    }
+
+    vec3 litColor = clamp(baseColor + ambient, 0.0, 1.0);
 
     vec3 finalColor =
         mix(
-            objectColor,
+            litColor,
             fogColor,
             fogFactor
         );
